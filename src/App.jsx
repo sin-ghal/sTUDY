4f20481fcaf107e3ed52dc1e651b289389f741b6
Your Name <anujsinghal443@gmail.com>
2026-05-20 21:18:26 +0530

    frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import "./App.css";
+import { Route,Routes } from "react-router-dom";
+import Home from "./pages/Home"
+
 
 function App() {
   return (
-   
+   <div>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+    </Routes>
+   </div>
   );
 }
 
